faa4ea81a51b0cbeaa9287afadff2333664abb1a
AMM00699 <amm00699@AMM00699s-Mac-mini.local>
2019-03-04 18:48:01 +0530

    Test case updated

--- components/App.test.js
+++ components/App.test.js
@@ -5,8 +5,8 @@ import App from './App';
 describe('Testing App component', () => {
   it('renders as expected', () => {
     const wrapper = shallow(<App receiver="Hello" />);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot({ updateSnapshot: true });
     wrapper.setProps({ receiver: 'Bye' });
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot({ updateSnapshot: true });
   });
 });
